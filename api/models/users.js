56490c4452390863709b3ab07ac312d8faff9a61
Simon <>
2022-07-02 10:44:00 -0300

    Commit

--- api/models/users.js
+++ api/models/users.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
+
 const userSchema = new mongoose.Schema(
     {
         name: {type: String, required: true},
         username: {type: String, lowercase: true, unique: true, required: true},
-        password: {type: String, required: true},
+        hash: {type: String},
+        salt: {type: String},
         access: {type: String, required: true}
     },
     {
